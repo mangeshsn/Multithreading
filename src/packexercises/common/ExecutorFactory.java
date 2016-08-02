36e82bb0f7bbac604b7ff9bb0ef5f29b1d7ea4d6
Narkhede <mangesh.Narkhede@capgemini.com>
2016-08-02 12:51:32 +0530

    THirD

--- src/packexercises/common/ExecutorFactory.java
+++ src/packexercises/common/ExecutorFactory.java
@@ -7,10 +7,10 @@ public class ExecutorFactory {
 
 	public static ExecutorService getExecutorService(){
 		// Starts one thread and executes more than one execute() in same thread.
-		return Executors.newSingleThreadExecutor();
+		//return Executors.newSingleThreadExecutor();
 		
 		// Starts two threads max.
-		//return Executors.newFixedThreadPool(3);
+		return Executors.newFixedThreadPool(3);
 		//return Executors.newFixedThreadPool(4);
 		//return Executors.newCachedThreadPool();
 	}
